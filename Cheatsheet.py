f2bf2fc5972ed016b9f8fb98b0cb70c285817fbf
Stephen Estrada <stephenestrada@Stephens-MacBook.local>
2016-06-08 17:06:39 -0400

    setup environement with inenv, create readme and gitignore

--- Cheatsheet.py
+++ Cheatsheet.py
@@ -1,14 +1,14 @@
-For BMO Hadoop Cluster:
-Python Files using Barnum and Faker must in the folder containing the Barnum and Faker files, currently:
-/home/mrains/cc_hadoop/venv/lib/python2.7/site-packages
-
-For each new session deactivate python 2.6 and activate 2.7:
-deactivate
-source ./venv/bin/activate
-
-How to find cpu configuration
-lscpu
-
-
-How to find kill processes:
-  dmesg  | egrep -i -B100 'killed process'
+# For BMO Hadoop Cluster:
+# Python Files using Barnum and Faker must in the folder containing the Barnum and Faker files, currently:
+# /home/mrains/cc_hadoop/venv/lib/python2.7/site-packages
+#
+# For each new session deactivate python 2.6 and activate 2.7:
+# deactivate
+# source ./venv/bin/activate
+#
+# How to find cpu configuration
+# lscpu
+#
+#
+# How to find kill processes:
+#   dmesg  | egrep -i -B100 'killed process'
